e15e2a7c10016e0eea1d42ea7ffe07aa6bf589d5
Ign6779 <ignacio.deeusebio.cobo@gmail.com>
2025-01-13 20:22:30 +0100

    fetching client token dynamically

--- keycloak/keycloak_client.go
+++ keycloak/keycloak_client.go
@@ -7,6 +7,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 type Config struct {
@@ -19,17 +20,55 @@ type Client struct {
 	config *config.Config
 }
 
+type TokenResponse struct {
+	AccessToken string `json:"access_token"`
+}
+
 func NewKeycloakClient(cfg *config.Config) *Client {
 	return &Client{config: cfg}
 }
 
+func (kc *Client) GetClientToken() (string, error) {
+	url := fmt.Sprintf("%s/realms/%s/protocol/openid-connect/token", kc.config.KeycloakURL, kc.config.KeycloakRealm)
+
+	data := "client_id=user-microservice&client_secret=Z7njfSA8YR7kDkftQMKjlqzwM1yqnKLK&grant_type=client_credentials"
+	req, err := http.NewRequest("POST", url, strings.NewReader(data))
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("Failed to get token: %s", resp.Status)
+	}
+
+	var tokenResponse TokenResponse
+	if err := json.NewDecoder(resp.Body).Decode(&tokenResponse); err != nil {
+		return "", err
+	}
+
+	return tokenResponse.AccessToken, nil
+}
+
 func (kc *Client) GetAllUsers() ([]models.User, error) {
+	token, err := kc.GetClientToken()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get token: %s", err)
+	}
+
 	url := fmt.Sprintf("%s/admin/realms/%s/users", kc.config.KeycloakURL, kc.config.KeycloakRealm)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Authorization", "Bearer "+kc.config.AdminToken)
+	req.Header.Set("Authorization", "Bearer "+token)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -58,12 +97,17 @@ func (kc *Client) GetAllUsers() ([]models.User, error) {
 }
 
 func (kc *Client) GetUserById(id string) (*models.User, error) {
+	token, err := kc.GetClientToken()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get token: %s", err)
+	}
+
 	url := fmt.Sprintf("%s/admin/realms/%s/users/%s", kc.config.KeycloakURL, kc.config.KeycloakRealm, id)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Authorization", "Bearer "+kc.config.AdminToken)
+	req.Header.Set("Authorization", "Bearer "+token)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -89,11 +133,16 @@ func (kc *Client) GetUserById(id string) (*models.User, error) {
 }
 
 func (kc *Client) RegisterUser(newUser models.User) error {
+	token, err := kc.GetClientToken()
+	if err != nil {
+		return fmt.Errorf("Failed to get token: %s", err)
+	}
+
 	url := fmt.Sprintf("%s/admin/realms/%s/users", kc.config.KeycloakURL, kc.config.KeycloakRealm)
 
 	body, _ := json.Marshal(newUser)
 	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(body))
-	req.Header.Set("Authorization", "Bearer "+kc.config.AdminToken)
+	req.Header.Set("Authorization", "Bearer "+token)
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := http.DefaultClient.Do(req)
@@ -111,12 +160,17 @@ func (kc *Client) RegisterUser(newUser models.User) error {
 // adding roles to the json bc the /users endpoint doesn't by default
 // do this in O(1) lmao
 func (kc *Client) GetUserRoles(id string) ([]string, error) {
+	token, err := kc.GetClientToken()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get token: %s", err)
+	}
+
 	url := fmt.Sprintf("%s/admin/realms/%s/users/%s/role-mappings/realm", kc.config.KeycloakURL, kc.config.KeycloakRealm, id)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Authorization", "Bearer "+kc.config.AdminToken)
+	req.Header.Set("Authorization", "Bearer "+token)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
